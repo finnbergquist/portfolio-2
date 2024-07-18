5e59e191c7efc8fe69780aee192c049d79121010
finnbergquist <finnyb38@gmail.com>
2024-07-18 15:37:31 -0400

    Update MainPage.js

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -3,8 +3,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
-import { resume } from '../../photos/resume.pdf';
 import './MainPage.css';
 
 const MainPage = () => {
@@ -15,18 +13,15 @@ const MainPage = () => {
         <p>Explore my projects and learn more about me.</p>
 
         {/* LinkedIn */}
-        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer" className="social-link">
+        <a href="https://www.linkedin.com/in/finn-bergquist/" target="_blank" rel="noopener noreferrer" className="social-link">
           <FontAwesomeIcon icon={faLinkedin} size="3x" />
         </a>
 
         {/* GitHub */}
-        <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer" className="social-link">
+        <a href="https://github.com/finnbergquist" target="_blank" rel="noopener noreferrer" className="social-link">
           <FontAwesomeIcon icon={faGithub} size="3x" />
         </a>
 
-        <a href="/path/to/your/resume.pdf" target="_blank" rel="noopener noreferrer" className="social-link">
-          <FontAwesomeIcon icon={faFilePdf} size="3x" />
-        </a>
       </div>
     </div>
   );
